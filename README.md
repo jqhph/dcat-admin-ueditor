bba295735093960a815d9299aa73803d603c69f6
Jiang qinghua <841324345@qq.com>
2019-06-21 11:22:28 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dcat-admin-ueditor
+dcat-admin extension
+======
+
+
---
+++ src/Form/Ueditor.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Dcat\Admin\Extension\UEditor\Form;
+
+use Dcat\Admin\Form\Field;
+use Illuminate\Support\Str;
+
+/**
+ * 百度在线编辑器
+ *
+ * User: jqh
+ * Date: 18-12-19
+ * Time: 下午4:42
+ */
+class UEditor extends Field
+{
+    protected static $js = [
+        '/vendor/dcat-admin-extensions/ueditor/ueditor.config.js',
+        '/vendor/dcat-admin-extensions/ueditor/ueditor.all.min.js',
+    ];
+
+    protected $options = [
+        // 编辑器默认高度
+        'initialFrameHeight' => 400,
+        'maximumWords' => 100000,
+        'serverUrl' => '',
+    ];
+
+    protected $view = 'ueditor::ueditor';
+
+    /**
+     * @var string
+     */
+    protected $disk;
+
+    /**
+     * 设置编辑器高度
+     *
+     * @param int $height
+     * @return $this
+     */
+    public function height(int $height)
+    {
+        $this->options['initialFrameHeight'] = $height;
+        return $this;
+    }
+
+    /**
+     * 设置上传接口
+     *
+     * @param string $url
+     * @return $this
+     */
+    public function server(string $url)
+    {
+        $this->options['serverUrl'] = url()->isValidUrl($url) ? $url : admin_base_path($url);
+
+        return $this;
+    }
+
+    /**
+     * @param string $disk
+     * @return $this
+     */
+    public function disk($disk)
+    {
+        $this->disk = $disk;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    protected function formatOptions()
+    {
+        if (empty($this->options['serverUrl'])) {
+            $this->server('/ueditor/serve');
+        }
+
+        return json_encode($this->options);
+    }
+
+    /**
+     * 初始化js
+     */
+    protected function setupScript()
+    {
+        $id = 'ueditor-'.Str::random(8);
+        $this->attribute('id', $id);
+
+        $opts = $this->formatOptions();
+
+        $cls = $this->getElementClassString().'_wrapper';
+
+        $this->script = <<<JS
+(function () {
+    var ue=UE.getEditor('$id', $opts);
+    ue.ready(function() {
+        ue.setContent($('.$cls').html());
+        ue.execCommand('serverparam', '_token', LA.token);
+        ue.execCommand('serverparam', 'disk', '{$this->disk}');
+    });
+})();
+JS;
+    }
+
+    /**
+     * Get the view variables of this field.
+     *
+     * @return array
+     */
+    public function variables()
+    {
+        return array_merge(parent::variables(), [
+            'homeUrl' => admin_asset('/vendor/dcat-admin-extensions/ueditor').'/',
+        ]);
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        $this->setupScript();
+
+        return parent::render();
+    }
+}
